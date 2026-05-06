5c060c33b27ea70f8f449547e6ff72ec7d38a2c2
jain akshat <akshatj592@gmail.com>
2026-05-06 11:09:23 +0530

    Endterm

---
+++ 1stquestion/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NameInput from "./components/NameInput";
+
+function App() {
+  return (
+    <div>
+      <NameInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ 1stquestion/src/components/nameinput.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function NameInput() {
+  const [name, setName] = useState("");
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Enter Your Name</h2>
+
+      <input
+        type="text"
+        placeholder="Type your name..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ padding: "10px", fontSize: "16px" }}
+      />
+
+      <h3 style={{ marginTop: "20px" }}>
+        Your Name: {name}
+      </h3>
+    </div>
+  );
+}
+
+export default NameInput;
